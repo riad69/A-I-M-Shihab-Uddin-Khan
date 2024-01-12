1d3f1c5ebe999043f32b46e75bcd9ad45773f893
shihabrafio <aimshihabuddinkhan@gmail.com>
2024-01-13 01:18:50 +0600

    Add flexbox layout and styling to App.css, import and use new components in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { Complete } from './components/complete';
+import { Completed } from './components/completed';
+import { Doing } from './components/doing';
+import { Incomplete } from './components/incomplete';
+import { Overdue } from './components/overdue';
+import { Todo } from './components/todo';
+import { Underreview } from './components/underreview';
 
 function App() {
   return (
     <div className="App">
-      
+      <Incomplete/>
+      <Todo/>
+      <Doing/>
+      <Underreview/>
+      <Completed/>
+      <Overdue/>
     </div>
   );
 }
