8148cb46b10f7be9b898f8384964f7a290238a93
shihabrafio <aimshihabuddinkhan@gmail.com>
2024-01-13 01:17:52 +0600

    Add Incomplete component

---
+++ src/components/incomplete.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Card } from './card'
+
+export const Incomplete = () => {
+  return (
+    <div>
+        <div className='flex'>
+            <h1 className='title'>Incomplete</h1>
+            <p className='number'>0</p>
+        </div>
+        <div className='scroll'>
+        <div>
+            {[...Array(12)].map((_, index) => (
+            <Card key={index} />
+            ))}
+        </div>
+        </div>
+    </div>
+  )
+}
