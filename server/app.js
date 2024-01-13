a49383c41af4ac88a1018add94c849411b1c1d63
shihabrafio <aimshihabuddinkhan@gmail.com>
2024-01-13 18:52:57 +0600

    Refactor app.js to use express.json middleware
    This commit refactors the app.js file to use the express.json middleware for parsing JSON data. This ensures that the server can handle JSON requests properly.

--- server/app.js
+++ server/app.js
@@ -5,9 +5,12 @@ const fileRoutes = require('./routes/fileRoutes');
 const app = express();
 const port = 3000;
 
+
 mongoose.connect('mongodb://localhost:27017/fileupload', { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.use('/files', fileRoutes); // Use the exported router correctly
+app.use(express.json());
+
+app.use('/files', fileRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
